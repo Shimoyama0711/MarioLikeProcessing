d0e416629876211d4396492252b1aaf227eb1871
Shimoyama <m19284@g.metro-cit.ac.jp>
2022-11-02 02:20:33 +0900

    イﾞェアアアア

--- src/Main.java
+++ src/Main.java
@@ -15,23 +15,36 @@ public class Main extends PApplet {
     @Override
     public void setup() {
         frameRate(60);
-
-        SoundFile sf = new SoundFile(this, "resource/sound/kumabachi.mp3");
-        sf.play();
+        playSound("kumabachi.mp3");
     }
+
+    int count = 0;
 
     @Override
     public void draw() {
-        background(255, 255, 255);
+        background(0, 0, 0);
 
         for (int x = 0; x <= width; x += 96) {
             for (int y = 0; y <= height; y += 96) {
-                int r = (int) random(0, 5);
+                int r = (count < 0) ? -1 : (int) random(0, 5);
                 String fileName = "resource/img/mario" + r + ".png";
                 PImage img = loadImage(fileName);
 
                 image(img, x, y, 32, 32);
             }
         }
+
+        count++;
+    }
+
+    @Override
+    public void mouseClicked() {
+        count = -5;
+        playSound("YEAH.mp3");
+    }
+
+    public void playSound(String fileName) {
+        SoundFile sf = new SoundFile(this, "resource/sound/" + fileName);
+        sf.play();
     }
 }
