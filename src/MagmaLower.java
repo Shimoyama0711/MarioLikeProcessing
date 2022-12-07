d4d5a9f5ab992e6d32f7e7038bd3db86f0e46a24
Ohwaki <m19232@g.metro-cit.ac.jp>
2022-12-07 16:05:26 +0900

    マグマの実装

---
+++ src/MagmaLower.java
@@ -0,0 +1,44 @@
+public class MagmaLower extends Block {
+
+    // public static boolean Hit;
+
+    /**
+     * Magma_lowerをデフォルトで生成します
+     */
+    public MagmaLower() {
+        FileName = "resource/img/magma2.png";
+        X = 0;
+        Y = 0;
+        Width = 32;
+        Height = 32;
+    }
+
+    /**
+     * Magma_lowerを32×32pxで座標を指定して描画します
+     *
+     * @param x Magma_lowerのX座標
+     * @param y Magma_lowerのY座標
+     */
+    public MagmaLower(float x, float y, float friction) {
+        FileName = "resource/img/magma2.png";
+        X = x;
+        Y = y;
+    }
+
+    /**
+     * Magma_lowerを解像度と座標を指定して描画します
+     *
+     * @param x Magma_lowerのX座標
+     * @param y Magma_lowerのY座標
+     * @param width Magma_lowerの横幅
+     * @param height Magma_lowerの縦幅
+     */
+    public MagmaLower(float x, float y, float width, float height) {
+        FileName = "resource/img/magma2.png";
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
+}
---
+++ src/MagmaUpper.java
@@ -0,0 +1,44 @@
+public class MagmaUpper extends Block {
+
+    // public static boolean Hit;
+
+    /**
+     * Magma_upperをデフォルトで生成します
+     */
+    public MagmaUpper() {
+        FileName = "resource/img/magma1.png";
+        X = 0;
+        Y = 0;
+        Width = 32;
+        Height = 32;
+    }
+
+    /**
+     * Magma_upperを32×32pxで座標を指定して描画します
+     *
+     * @param x Magma_upperのX座標
+     * @param y Magma_upperのY座標
+     */
+    public MagmaUpper(float x, float y, float friction) {
+        FileName = "resource/img/magma1.png";
+        X = x;
+        Y = y;
+    }
+
+    /**
+     * Magma_upperを解像度と座標を指定して描画します
+     *
+     * @param x Magma_upperのX座標
+     * @param y Magma_upperのY座標
+     * @param width Magma_upperの横幅
+     * @param height Magma_upperの縦幅
+     */
+    public MagmaUpper(float x, float y, float width, float height) {
+        FileName = "resource/img/magma1.png";
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
+}
